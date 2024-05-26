4f6cca3fc59f97051aa0ec7edb778c26f101629d
Raphael Guye <raphael.guye@bit.admin.ch>
2024-05-26 18:24:48 +0200

    Fix the criteria for the new template and run the analysis for 2024-05-04 and 2024-05-25

--- module/model/criteria.rb
+++ module/model/criteria.rb
@@ -1,27 +1,27 @@
 Criteria = Struct.new(:name, :abreviation, :threshold)
 
 criteria_df = 2.0
-criteria_co = 2.0
-criteria_fm = 1.6
-criteria_fw = -1
+criteria_cdp = 2.0
+criteria_fm = 12.0
+criteria_fw = 12.0
 criteria_sm = 2.3
 criteria_bm = 3.0
 criteria_acro = 12.0
-criteria_fall = 3.0
 
 $criterias_formations = [
-    Criteria.new("Dance Figure", "DF", criteria_df),
-    Criteria.new("Choreography", "CO", criteria_co),
-    Criteria.new("?", "FM", criteria_fm),
-    # Criteria.new("?", "FM", 1.6),
+    Criteria.new("Dance Figure", "DF1", criteria_df),
+    Criteria.new("Choreography", "CDP1", criteria_cdp),
+    Criteria.new("?", "FWM1", criteria_fm),
+    Criteria.new("?", "FWL1", criteria_fw),
     Criteria.new("Small mistakes", "sm", criteria_sm),
     Criteria.new("Big mistakes", "bm", criteria_bm),
 ]
 
 $criterias_couples_dance = [
-    Criteria.new("Dance Figure", "DF", criteria_df), 
-    Criteria.new("Choreography", "CO", criteria_co), 
-    Criteria.new("Foot Man", "FM", criteria_fm), 
+    Criteria.new("Dance Figure", "DF1", criteria_df), 
+    Criteria.new("Choreography", "CDP1", criteria_cdp), 
+    Criteria.new("Foot Man", "FWM1", criteria_fm), 
+    Criteria.new("Foot Man", "FWL1", criteria_fw), 
     Criteria.new("Small mistakes", "sm", criteria_sm),
     Criteria.new("Big mistakes", "bm", criteria_bm),
 ]
@@ -33,6 +33,5 @@ $criterias_couples_acro = [
     Criteria.new("Acro 4", "Acro 4", criteria_acro), 
     Criteria.new("Acro 5", "Acro 5", criteria_acro), 
     Criteria.new("Acro 6", "Acro 6", criteria_acro), 
-    Criteria.new("Fall", "fall", criteria_fall),
     Criteria.new("Big mistake", "bm", criteria_bm),
 ]
